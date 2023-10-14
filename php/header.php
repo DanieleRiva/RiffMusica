2610248dbc794fe7885e681acca65ca9d6af9a8c
Daniele Riva <danieleriva03@gmail.com>
2023-10-14 19:54:04 +0200

    Header show/hide fixed, remove Docenti link

--- php/header.php
+++ php/header.php
@@ -17,13 +17,11 @@
             <h2>Riff Musica</h2>
 
             <a onclick="hamburgerButton()">
-                <span id="hamburger-btn" class="material-symbols-outlined">
-                    menu
-                </span>
+                <span id="hamburger-btn" class="material-symbols-outlined">menu</span>
             </a>
         </div>
 
-        <div id="side-menu">
+        <div class="side-menu">
             <ul class="nav">
                 <li <?php if (!isset($_GET["page"])) { ?> id="current-page" <?php } ?> onclick="closeSidebar()">
                     <a href="index.php" <?php if (isset($_GET["page"]) && $_GET["page"] != "home") { ?> id="a-header" <?php } ?> class="nav-link nav-link-home px-2 transition-a">
@@ -35,12 +33,6 @@
                     <a href="?page=corsi" <?php if (!isset($_GET["page"]) || isset($_GET["page"]) && $_GET["page"] != "corsi") { ?> id="a-header" <?php } ?> class="nav-link nav-link-corsi px-2 transition-a">
                         <img id="header-icon" src="img/header/icons/static/corsi.png" alt="" width="32px">
                         Corsi
-                    </a>
-                </li>
-                <li <?php if (isset($_GET["page"]) && $_GET["page"] == "docenti") { ?> id="current-page" <?php } ?> onclick="closeSidebar()">
-                    <a href="?page=docenti" <?php if (!isset($_GET["page"]) || isset($_GET["page"]) && $_GET["page"] != "docenti") { ?> id="a-header" <?php } ?> class="nav-link nav-link-docenti px-2 transition-a">
-                        <img id="header-icon" src="img/header/icons/static/docenti.png" alt="" width="32px">
-                        Docenti
                     </a>
                 </li>
                 <li <?php if (isset($_GET["page"]) && $_GET["page"] == "media") { ?> id="current-page" <?php } ?> onclick="closeSidebar()">
@@ -57,7 +49,6 @@
                 </li>
             </ul>
         </div>
-
 
         <!-- <img src="img/header/logo.webp" id="logo" alt="">
 
@@ -98,18 +89,36 @@
 
 
 
+
     <script>
-        const sideMenu = document.getElementById("side-menu");
+        const sideMenu = document.getElementsByClassName("side-menu")[0];
         const hamburgerBtn = document.getElementById("hamburger-btn");
 
         function hamburgerButton() {
-            if (sideMenu.style.display === "block") {
-                sideMenu.style.display = "none";
-                hamburgerBtn.textContent = "menu";
-            } else {
-                sideMenu.style.display = "block";
+            sideMenu.classList.toggle("active");
+            hamburgerBtn.classList.toggle("active");
+
+            if (hamburgerBtn.textContent.trim() == "menu") {
                 hamburgerBtn.textContent = "close";
+                // sideMenu.classList.remove("hidden");
+                // sideMenu.classList.add("visible");
+            } else {
+                hamburgerBtn.textContent = "menu";
+                // sideMenu.classList.remove("visible");
+                // sideMenu.classList.add("hidden");
             }
+
+            // if (sideMenu.style.display === "block") {
+            //     sideMenu.classList.add("visible");
+            //     sideMenu.classList.remove("hidden");
+            //     // sideMenu.style.display = "none";
+            //     hamburgerBtn.textContent = "menu";
+            // } else {
+            //     sideMenu.classList.add("hidden");
+            //     sideMenu.classList.remove("visible");
+            //     // sideMenu.style.display = "block";
+            //     hamburgerBtn.textContent = "close";
+            // }
         }
 
         function closeSidebar() {
