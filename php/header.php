a1e7e1b322b203d619cbd288554080e2ab2442f0
Daniele Riva <danieleriva03@gmail.com>
2023-10-16 14:59:18 +0200

    Changes to mobile navbar

--- php/header.php
+++ php/header.php
@@ -13,41 +13,49 @@
 <body>
     <header id="header">
 
-        <div class="menu">
-            <h2>Riff Musica</h2>
+        <div class="topbar">
+            <h1>Riff Musica</h1>
 
             <a onclick="hamburgerButton()">
                 <span id="hamburger-btn" class="material-symbols-outlined">menu</span>
             </a>
         </div>
 
-        <div class="side-menu">
-            <ul class="nav">
-                <li <?php if (!isset($_GET["page"])) { ?> id="current-page" <?php } ?> onclick="closeSidebar()">
-                    <a href="index.php" <?php if (isset($_GET["page"]) && $_GET["page"] != "home") { ?> id="a-header" <?php } ?> class="nav-link nav-link-home px-2 transition-a">
-                        <img id="header-icon" src="img/header/icons/static/home.png" alt="" width="32px">
-                        Home
-                    </a>
-                </li>
-                <li <?php if (isset($_GET["page"]) && $_GET["page"] == "corsi") { ?> id="current-page" <?php } ?> onclick="closeSidebar()">
-                    <a href="?page=corsi" <?php if (!isset($_GET["page"]) || isset($_GET["page"]) && $_GET["page"] != "corsi") { ?> id="a-header" <?php } ?> class="nav-link nav-link-corsi px-2 transition-a">
-                        <img id="header-icon" src="img/header/icons/static/corsi.png" alt="" width="32px">
-                        Corsi
-                    </a>
-                </li>
-                <li <?php if (isset($_GET["page"]) && $_GET["page"] == "media") { ?> id="current-page" <?php } ?> onclick="closeSidebar()">
-                    <a href="?page=media" <?php if (!isset($_GET["page"]) || isset($_GET["page"]) && $_GET["page"] != "media") { ?> id="a-header" <?php } ?> class="nav-link nav-link-media px-2 transition-a">
-                        <img id="header-icon" src="img/header/icons/static/media.png" alt="" width="32px">
-                        Media
-                    </a>
-                </li>
-                <li <?php if (isset($_GET["page"]) && $_GET["page"] == "contatti") { ?> id="current-page" <?php } ?> onclick="closeSidebar()">
-                    <a href="?page=contatti" <?php if (!isset($_GET["page"]) || isset($_GET["page"]) && $_GET["page"] != "contatti") { ?> id="a-header" <?php } ?> class="nav-link nav-link-contatti px-2 transition-a">
-                        <img id="header-icon" src="img/header/icons/static/contatti.png" alt="" width="32px">
-                        Contatti
-                    </a>
-                </li>
-            </ul>
+        <div class="menu-wrapper">
+
+            <div class="backdrop" onclick="hamburgerButton()"></div>
+
+            <div class="side-menu">
+                <img id="side-menu-logo" src="img/header/logo.webp" alt="">
+
+                <ul class="nav">
+                    <li <?php if (!isset($_GET["page"])) { ?> id="current-page" <?php } ?> onclick="hamburgerButton()">
+                        <a href="index.php" <?php if (isset($_GET["page"]) && $_GET["page"] != "home") { ?> id="a-header" <?php } ?> class="nav-link nav-link-home px-2 transition-a">
+                            <img id="header-icon" src="img/header/icons/static/home.png" alt="" width="32px">
+                            Home
+                        </a>
+                    </li>
+                    <li <?php if (isset($_GET["page"]) && $_GET["page"] == "corsi") { ?> id="current-page" <?php } ?> onclick="hamburgerButton()">
+                        <a href="?page=corsi" <?php if (!isset($_GET["page"]) || isset($_GET["page"]) && $_GET["page"] != "corsi") { ?> id="a-header" <?php } ?> class="nav-link nav-link-corsi px-2 transition-a">
+                            <img id="header-icon" src="img/header/icons/static/corsi.png" alt="" width="32px">
+                            Corsi
+                        </a>
+                    </li>
+                    <li <?php if (isset($_GET["page"]) && $_GET["page"] == "media") { ?> id="current-page" <?php } ?> onclick="hamburgerButton()">
+                        <a href="?page=media" <?php if (!isset($_GET["page"]) || isset($_GET["page"]) && $_GET["page"] != "media") { ?> id="a-header" <?php } ?> class="nav-link nav-link-media px-2 transition-a">
+                            <img id="header-icon" src="img/header/icons/static/media.png" alt="" width="32px">
+                            Media
+                        </a>
+                    </li>
+                    <li <?php if (isset($_GET["page"]) && $_GET["page"] == "contatti") { ?> id="current-page" <?php } ?> onclick="hamburgerButton()">
+                        <a href="?page=contatti" <?php if (!isset($_GET["page"]) || isset($_GET["page"]) && $_GET["page"] != "contatti") { ?> id="a-header" <?php } ?> class="nav-link nav-link-contatti px-2 transition-a">
+                            <img id="header-icon" src="img/header/icons/static/contatti.png" alt="" width="32px">
+                            Contatti
+                        </a>
+                    </li>
+                </ul>
+            </div>
+
         </div>
 
         <!-- <img src="img/header/logo.webp" id="logo" alt="">
@@ -91,38 +99,20 @@
 
 
     <script>
-        const sideMenu = document.getElementsByClassName("side-menu")[0];
+        const menu = document.getElementsByClassName("menu-wrapper")[0];
         const hamburgerBtn = document.getElementById("hamburger-btn");
 
         function hamburgerButton() {
-            sideMenu.classList.toggle("active");
+            menu.classList.toggle("active");
             hamburgerBtn.classList.toggle("active");
 
             if (hamburgerBtn.textContent.trim() == "menu") {
                 hamburgerBtn.textContent = "close";
-                // sideMenu.classList.remove("hidden");
-                // sideMenu.classList.add("visible");
+                document.body.style.position = "fixed";
             } else {
                 hamburgerBtn.textContent = "menu";
-                // sideMenu.classList.remove("visible");
-                // sideMenu.classList.add("hidden");
+                document.body.style.position = "unset";
             }
-
-            // if (sideMenu.style.display === "block") {
-            //     sideMenu.classList.add("visible");
-            //     sideMenu.classList.remove("hidden");
-            //     // sideMenu.style.display = "none";
-            //     hamburgerBtn.textContent = "menu";
-            // } else {
-            //     sideMenu.classList.add("hidden");
-            //     sideMenu.classList.remove("visible");
-            //     // sideMenu.style.display = "block";
-            //     hamburgerBtn.textContent = "close";
-            // }
-        }
-
-        function closeSidebar() {
-            sideMenu.style.display = "none";
         }
     </script>
 </body>
