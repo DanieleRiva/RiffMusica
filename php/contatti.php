d448c777591522f37019883eebff042e267c8775
Daniele Riva <danieleriva03@gmail.com>
2023-11-24 18:17:45 +0100

    Contacts page

---
+++ php/contatti.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Riff Musica - Contatti</title>
+</head>
+
+<body>
+    <div class="body-wrapper">
+
+        <div class="section-one" id="section-scroll">
+
+            <div class="centered">
+                <div class="section-content">
+                    <h1>RESTA SEMPRE <span style="color: rgb(var(--color-secondary));">AGGIORNATO</span></h1>
+                    <p>Seguici sui social per restare a conoscenza di tutti gli ultimi eventi e novità.</p>
+
+                    <div class="socials" data-aos="slide-down">
+
+                        <a href="https://www.youtube.com/channel/UCWnCmpXYSeja13ZZdfLoPPA/featured" class="social-icon social-icon--youtube" target="_blank">
+                            <i class="fa fa-youtube"></i>
+                            <div class="tooltip">YouTube</div>
+                        </a>
+
+                        <a href="https://www.instagram.com/riff_associazione_musicale/" class="social-icon social-icon--instagram" target="_blank">
+                            <i class="fa fa-instagram"></i>
+                            <div class="tooltip">Instagram</div>
+                        </a>
+
+                        <a href="https://www.facebook.com/riffcassago" class="social-icon social-icon--facebook" target="_blank">
+                            <i class="fa fa-facebook"></i>
+                            <div class="tooltip">Facebook</div>
+                        </a>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="section-two" id="section-scroll">
+            <div class="custom-shape-divider-top-1697795202">
+                <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+                    <path d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z" class="shape-fill"></path>
+                </svg>
+            </div>
+
+            <div class="centered">
+                <div class="section-content">
+                    <h1>PER QUALSIASI <span style="color: rgb(var(--color-secondary));">INFORMAZIONE</span></h1>
+                    <p>Chiamaci, oppure scrivici su Whatsapp:</p>
+
+                    <div class="lezione-gratuita">
+                        <!-- <p>Chiamaci: </p> -->
+                        <a id="a-header" class="section-btn" href="tel:+393294686229">3294686229</a>
+                        <!-- <p>Oppure: </p> -->
+                        <a href="https://wa.me/393294686229" class="social-icon social-icon--whatsapp" target="_blank">
+                            <i class="fa fa-whatsapp"></i>
+                            <div class="tooltip">Whatsapp</div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="section-one" id="section-scroll">
+            <div class="custom-shape-divider-top-1697795202">
+                <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+                    <path d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z" class="shape-fill"></path>
+                </svg>
+            </div>
+
+            <div class="centered">
+                <div class="section-content">
+                    <h1>LE NOSTRE <span style="color: rgb(var(--color-secondary));">SEDI</span></h1>
+
+                    <div class="maps">
+
+                        <div class="map">
+                            <p>Cassago Brianza (LC): Vicolo Cascinetta 10</p> <br>
+                            <iframe class="home-map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2784.535666769543!2d9.289776976998562!3d45.74040607107981!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4786a52a385430bf%3A0xa4d5a74b9ec441b1!2sAssociazione%20musicale%20Riff!5e0!3m2!1sit!2sit!4v1700585711034!5m2!1sit!2sit" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                        </div>
+
+                        <div class="map">
+                            <p>Dolzago (LC): Via Corsica, 12A</p> <br>
+                            <iframe class="home-map" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d11132.454930971655!2d9.3410198!3d45.7689109!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4786a6662afc5d09%3A0x69f44f7b7b6ddab5!2sBiblioteca%20Comunale!5e0!3m2!1sit!2sit!4v1700845859718!5m2!1sit!2sit" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                        </div>
+
+                        <div class="map">
+                            <p>Albese Con Cassano (CO): Strada Vicinale</p> <br>
+                            <iframe class="home-map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2782.071493721567!2d9.1528678!3d45.789795399999996!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47869f1c1f7f808b%3A0xe5852ec7844831da!2sStr.%20Vicinale%20di%20Nibitt%2C%202%2C%2022032%20Albese%20Con%20Cassano%20CO!5e0!3m2!1sit!2sit!4v1700845869506!5m2!1sit!2sit" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</body>
+
+</html>
--- php/home.php
+++ php/home.php
@@ -58,7 +58,7 @@
             </div>
         </div>
 
-        <div class="section-two">
+        <div class="section-two" id="section-scroll">
             <div class="custom-shape-divider-top-1697795202">
                 <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
                     <path d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z" class="shape-fill"></path>
@@ -67,7 +67,7 @@
 
             <div class="centered">
                 <div class="section-content">
-                    <h1>PRENOTA ORA LA TUA<br>LEZIONE DI <span style="color: rgb(var(--color-secondary));">PROVA GRATUITA</span> SENZA IMPEGNO</span> </h1>
+                    <h1>PRENOTA ORA LA TUA<br>LEZIONE DI <span style="color: rgb(var(--color-secondary));">PROVA GRATUITA</span> SENZA IMPEGNO</h1>
                     <p>Chiamaci, oppure scrivici su Whatsapp:</p>
 
                     <div class="lezione-gratuita">
@@ -83,7 +83,7 @@
             </div>
         </div>
 
-        <div class="section-one" id="section-scroll">
+        <div class="section-one">
             <div class="custom-shape-divider-top-1697795202">
                 <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
                     <path d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z" class="shape-fill"></path>
